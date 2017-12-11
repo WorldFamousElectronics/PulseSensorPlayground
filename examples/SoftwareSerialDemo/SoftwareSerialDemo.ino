d1432c812adfe16c76b4a54edac7f3c10e345400
biomurph <biomurph@gmail.comr544rre>
2017-12-11 14:20:34 -0500

    Added tutorial links to the examples

--- examples/SoftwareSerialDemo/SoftwareSerialDemo.ino
+++ examples/SoftwareSerialDemo/SoftwareSerialDemo.ino
@@ -3,8 +3,8 @@
    using an interrupt service routine and
    using SoftwareSerial for Serial output.
 
-   Here is a link to the tutorial
-   https://pulsesensor.com/pages/software-serial-example
+   Here is a link to the tutorial that discusses this coe
+   https://pulsesensor.com/pages/getting-advanced
 
    Copyright World Famous Electronics LLC - see LICENSE
    Contributors:
